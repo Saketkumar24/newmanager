b2d1eb7dca89b488ea0d6432bda1695aa603de5d
Saketkumar24 <saketc112@gmail.com>
2025-05-15 02:30:07 +0530

    Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import { Routes } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import Signup from "./Pages/Signup.jsx"
-import { Route } from "react-router-dom"
 import Login from './Pages/Login.jsx'
 import Dashboard from './Pages/Dashboard.jsx'
 import ProjectsPage from './Pages/CreateProject.jsx'
@@ -10,19 +9,27 @@ import ProjectDetailPage from './Pages/ProjectDetailPage.jsx'
 import Navbar from './Components/Navbar.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const token = localStorage.getItem('token')
 
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path='/projects' element={<Dashboard />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/register" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/create-project" element={<ProjectsPage />} />
-        <Route path="/project/:projectId" element={<ProjectDetailPage />} />
-        <Route path="/" element={<ProjectDetailPage />} />
+        {token ? (
+          <>
+            <Route path='/projects' element={<Dashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/create-project" element={<ProjectsPage />} />
+            <Route path="/project/:projectId" element={<ProjectDetailPage />} />
+            <Route path="/" element={<Dashboard />} />
+          </>
+        ) : (
+          <>
+            <Route path="/register" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Login />} />
+          </>
+        )}
       </Routes>
     </>
   )
