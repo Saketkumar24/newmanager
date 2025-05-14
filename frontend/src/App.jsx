a18e85422a1e14f52c60b6fe4f8d7d5f610c207d
Saketkumar24 <saketc112@gmail.com>
2025-05-15 02:40:28 +0530

    Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,12 +21,13 @@ function App() {
             <Route path="/create-project" element={<ProjectsPage />} />
             <Route path="/project/:projectId" element={<ProjectDetailPage />} />
             <Route path="/" element={<Dashboard />} />
-          </>
-        ) : (
-          <>
             <Route path="/register" element={<Signup />} />
             <Route path="/login" element={<Login />} />
             <Route path="*" element={<Login />} />
+          </>
+        ) : (
+          <>
+
           </>
         )}
       </Routes>
